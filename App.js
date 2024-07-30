f1fca295c2bf0ee83401982c6dae467063037f70
LamTP3 <phuclamtran2810@gmail.com>
2024-07-30 09:48:48 +0700

    #1 video 7 layout & flexbox Thach Pham Dev

--- App.js
+++ App.js
@@ -1,39 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <Text style={styles.myText}>
-        Open up App.js to start working on your app!
-      </Text>
-      <Text style={styles2.myText}>Test Text</Text>
-      <StatusBar style="auto" />
-      <Button title="Press me" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="your todo" style={styles.textInput} />
+        <Button title="Add todo" />
+      </View>
+      <View>
+        <Text>The to do list</Text>
+      </View>
     </View>
   );
 }
-const styles2 = StyleSheet.create({
-  myText: {
-    margin: 10,
-    borderWidth: 2,
-    borderColor: "red",
-    padding: 10,
-  },
-});
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  myText: {
-    margin: 10,
-    borderWidth: 2,
-    borderColor: "red",
     padding: 10,
+    paddingTop: 100,
+  },
+  inputContainer: {
+    flexDirection: "row",
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: "blue",
+    padding: 8,
+    marginRight: 8,
+    width: "70%",
   },
 });
